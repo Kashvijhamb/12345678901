27b3d7aa2591a169f1f4aa15b09596d155f6fc7b
Kashvijhamb <86904200+Kashvijhamb@users.noreply.github.com>
2023-01-16 15:32:59 +0530

    Add files via upload

--- living_room.js
+++ living_room.js
@@ -1,4 +1,5 @@
 img = "";
+status = "";
 function preload()
 {
     img = loadImage('livingroom.png');
@@ -7,6 +8,9 @@ function setup()
 {
     canvas= createCanvas(640, 420);
     canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 function draw()
 {
@@ -17,4 +21,16 @@ function draw()
     stroke("#FF0000");
     rect(30,60,450,350);
 }
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+  }
+  function gotResult(error, results) 
+  {
+    if (error) {
+      console.log(error);
+    }
+    console.log(results);
+  }  
 
--- road.js
+++ road.js
@@ -7,6 +7,9 @@ function setup()
 {
     canvas= createCanvas(640, 420);
     canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 function draw()
 {
@@ -17,4 +20,16 @@ function draw()
     stroke("#FF0000");
     rect(30,60,450,350);
 }
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+  }
+  function gotResult(error, results) 
+  {
+    if (error) {
+      console.log(error);
+    }
+    console.log(results);
+  }  
 
